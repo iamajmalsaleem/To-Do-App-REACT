e03651dcd071931e0dd86e40709e17df4626acfd
Ajmal_Saleem <ajmalsaleem84@gmail.com>
2021-10-21 18:03:03 +0530

    Phase I :
    1. Initialize the React app
    2.  Render an <App /> component
    3. Create the <App /> component from scratch
    4. Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+           <input type="checkbox" />
+           <span> I want a bike</span><br></br>
+             <input type="checkbox" />
+             <span> I want a car</span><br></br>
+               <input type="checkbox" />
+               <span> I want a boat</span><br></br>
+           
+        </div>
+      )
+}
+
+
+export default App 
