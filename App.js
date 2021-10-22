7176546c56d408d0c9b841b296683dfc327a1b9c
Ajmal_Saleem <ajmalsaleem84@gmail.com>
2021-10-22 18:09:28 +0530

    Phase II : 1. Change the input/span combo to be a new component called <TodoItem />. 2. Style up the page

--- App.js
+++ App.js
@@ -2,15 +2,13 @@ import React from "react"
 
 function App() {
     return (
-        <div>
-           <input type="checkbox" />
-           <span> placeholder text1 </span><br></br>
-             <input type="checkbox" />
-             <span> placeholder text2</span><br></br>
-               <input type="checkbox" />
-               <span> placeholder text3</span><br></br>
-           
-        </div>
+          <div className = "todo-list">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+          </div>
+    )
       )
 }
 
---
+++ TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const TodoItem = () => 
+         ( <div className = "todo-item">
+              <p></p>
+              <input type="checkbox" />
+              <span>  Placeholder text here</span>
+              <p></p>
+           </div>   
+          )
+            
+                       
+export default TodoItem                       
