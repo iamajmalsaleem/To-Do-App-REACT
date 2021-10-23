3307a5f7c15ad74cfa3d277e606d918d580a1097
Ajmal_Saleem <ajmalsaleem84@gmail.com>
2021-10-23 13:40:50 +0530

    Phase II (cont..)  :
    More styling done

--- TodoItem.js
+++ TodoItem.js
@@ -1,13 +1,14 @@
 import React from "react"
 
 const TodoItem = () => 
-         ( <div className = "todo-item">
-              <p></p>
-              <input type="checkbox" />
-              <span>  Placeholder text here</span>
-              <p></p>
-           </div>   
-          )
+        ( <div>
+              <div className = "todo-item">
+                <input type="checkbox" />
+                <span>Placeholder text here</span>
+              </div>
+            <br/> 
+          </div>   
+         )
             
                        
 export default TodoItem                       
