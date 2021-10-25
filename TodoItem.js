55adbdba1e368ce0a04d6e5665ededfe37c737d7
Ajmal_Saleem <ajmalsaleem84@gmail.com>
2021-10-25 10:36:35 +0530

    PHASE V :
    Get rid of our warning about not having an onChange on our input. For now, the function that runs onChange can simply console.log something.

--- TodoItem.js
+++ TodoItem.js
@@ -3,7 +3,10 @@ import React from "react"
 const TodoItem = () => 
         ( <div>
               <div className = "todo-item">
-                <input type="checkbox" checked = {props.task.completed} />
+                <input type="checkbox" 
+				       checked = {props.task.completed}
+                       onChange = {()=> console.log("CHANGED !")}
+			   />
                 <span>{props.task.text}</span>
               </div>
             <br/> 
